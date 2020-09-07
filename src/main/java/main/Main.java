6e9aeae91014e5a22262e9739def19a3dbb88a26
Dennis <99052183@mydavinci.nl>
2020-09-07 12:16:21 +0200

    cube now rotates!!!

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,7 @@ import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glColor3f;
+import static org.lwjgl.opengl.GL11.glRotatef;
 
 import gfx.Mesh;
 import glfw.Window;
@@ -38,6 +39,7 @@ public class Main
     private void render()
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glRotatef((float)Math.toRadians(20), 1, 1, 1);
         glColor3f(0, 0, 1);
         Mesh.CUBE.render();
     }
